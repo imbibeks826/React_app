109cd4d8bb0ae75fa490b5705f1ff21c4d756984
Bibekananda Sahoo <imbibeks826@gmail.com>
2026-02-04 00:50:23 +0530

    Removed testing code

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = React.createElement('h1',{key: 'header'},'Hello, Bibek!');
+const element = React.createElement('div',{key: 'header', style:{display:'flex', flex:1, height: '100vh', backgroundColor:'black', justifyContent:'center', alignItems:'center'}},React.createElement('h1',{key: 'h1', style:{color: 'white'}},'Hello, Bibek!'));
 const content = ReactDOM.createRoot(document.getElementById('root'));
-let List = React.createElement(
-        'div', {key :'listContainer'},
-        Array.from({length: 5}).map((_,i)=> React.createElement('li',{key:`${i}`},`Item ${i}`))
-    )
+// let List = React.createElement(
+//         'div', {key :'listContainer'},
+//         Array.from({length: 5}).map((_,i)=> React.createElement('li',{key:`${i}`},`Item ${i}`))
+//     )
 
-const rootDiv = React.createElement('div',{key: 'parent'},[element, List]);
+const rootDiv = React.createElement('div',{key: 'parent'},[element]);
 content.render(rootDiv);
