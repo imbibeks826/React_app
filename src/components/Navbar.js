60bc9d181d238ea304b8521bfd681694dc9e87f8
Bibekananda Sahoo <imbibeks826@gmail.com>
2026-03-23 15:52:18 +0530

    MyApp changed to BS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router';
 const Navbar = () => {
     return(
         <nav style={{display:'flex', justifyContent:'space-between', alignItems:'center', padding: '10px 20px', backgroundColor: '#333', color: 'white'}}>
-            <div style={{fontSize: '24px', fontWeight: 'bold'}}><NavLink to='/' style={{color: 'white', textDecoration: 'none'}}>MyApp</NavLink></div>
+            <div style={{fontSize: '24px', fontWeight: 'bold', width:'30px', backgroundColor:'blue', borderRadius:'8px', padding:'10px'}}><NavLink to='/' style={{color: 'white', textDecoration: 'none'}}>BS</NavLink></div>
             <ul style={{display: 'flex', listStyle: 'none', margin: 0, padding: 0}}>
                 <li style={{margin: '0 10px'}}><NavLink to="/" style={{color: 'white', textDecoration: 'none'}}>Home</NavLink></li>
                 <li style={{margin: '0 10px'}}><NavLink to="/about" style={{color: 'white', textDecoration: 'none'}}>About</NavLink></li>
